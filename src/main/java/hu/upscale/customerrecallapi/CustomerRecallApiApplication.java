04581da681cee4e308c14bb582fe7ced559df901
tamasUpScale <Tamas.Kiss.2@ext.otpbank.hu>
2023-02-23 16:12:35 +0100

    Initial commit

---
+++ src/main/java/hu/upscale/customerrecallapi/CustomerRecallApiApplication.java
@@ -0,0 +1,13 @@
+package hu.upscale.customerrecallapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRecallApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRecallApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/upscale/customerrecallapi/CustomerRecallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.upscale.customerrecallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRecallApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
